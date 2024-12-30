28d58ab4935d68a39ec87ca17ef8f571d9cc7a4d
satyendra_chaudhary <satyendrachaudhary38@gmail.com>
2024-12-30 14:42:00 +0530

    ok

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-// let audioPlay = document.getElementById("audio");
-// let clock = document.querySelector(".clock");
-// let left = document.querySelector(".left");
-// let right = document.querySelector(".right");
-// let welcome = document.querySelector(".welcome");
-// let welcome2 = document.querySelector(".welcome2");
-// let nameW = document.querySelector(".name");
+let audioPlay = document.getElementById("audio");
+let front = document.querySelector(".front");
+let back = document.querySelector(".back");
+let clock = document.querySelector(".clock");
+let left = document.querySelector(".left");
+let right = document.querySelector(".right");
+let welcome = document.querySelector(".welcome");
+let welcome2 = document.querySelector(".welcome2");
+let nameW = document.querySelector(".name");
 
-// clock.addEventListener("click", () => {
-//     audioPlay.play();
-//     clock.style.display = "none";
-//     left.classList.add("leftA");
-//     right.classList.add("rightA");
-//     left.classList.remove("leftt");
-//     right.classList.remove("rightt");
-//     setTimeout(() => { welcome.style.display = "block"; welcome2.style.display = "block"; nameW.classList.add = "animate__animated animate__swing animate__infinite	infinite animate__slower	3s"; }, 4000)
+clock.addEventListener("click", () => {
+    audioPlay.play();
+    clock.style.display = "none";
+    left.classList.add("leftA");
+    right.classList.add("rightA");
+    left.classList.remove("leftt");
+    right.classList.remove("rightt");
+    setTimeout(() => { front.style.display = "none"; back.classList.remove("back") }, 4000)
 
-// })
+})
 
 // name find
 let send = document.querySelector(".send");
