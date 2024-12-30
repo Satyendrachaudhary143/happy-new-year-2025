00b25593ffd23fef790f3706f2d53b7a305a1c2a
satyendra_chaudhary <satyendrachaudhary38@gmail.com>
2024-12-30 18:53:00 +0530

    share functionality

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let right = document.querySelector(".right");
 let welcome = document.querySelector(".welcome");
 let welcome2 = document.querySelector(".welcome2");
 let nameW = document.querySelector(".name");
-
+// clock click
 clock.addEventListener("click", () => {
     audioPlay.play();
     clock.style.display = "none";
@@ -25,6 +25,8 @@ let userNameFind = document.querySelector(".nameU");
 let userName;
 let nameU = document.querySelector(".inputUser");
 let mwish = document.querySelector(".mwish");
+let share = document.querySelector(".share");
+
 nameU.addEventListener("input", (e) => {
     userName = e.target.value;
 
@@ -41,7 +43,43 @@ send.addEventListener("click", () => {
         userNameFind.innerHTML = userName;
         mwish.innerHTML = `${userName} की तरफ से आपको और आपके परिवार को नववर्ष
                         2025 की हार्दिक शुभकामनायें !!`
-        console.log(userName);
+        send.style.display = "none";
+        nameU.style.display = "none";
+        share.style.display = "block";
 
     }
-})  
+})
+
+// share functionalty
+
+share.addEventListener("click", () => {
+    if (navigator.share) {
+        navigator.share({
+            title: 'New Year 2025',
+            text: `Happy New Year 2025${userName}`,
+            url: `https://satyendrachaudhary143.github.io/happyNewYear/?name=${userName}`,
+            // url: `http://127.0.0.1:5500/index.html?name=${userName}`
+        })
+            .then(() => console.log('Successful share'))
+            .catch((error) => console.log('Error sharing', error));
+
+    }
+})
+
+// params fanctionalty
+// Assume the URL is: https://example.com/?name=John&age=30
+
+// Step 1: Get the full URL
+const url = window.location.href;
+
+// Step 2: Create a URL object
+const urlObj = new URL(url);
+
+// Step 3: Use URLSearchParams to extract parameters
+const params = new URLSearchParams(urlObj.search);
+
+// Step 4: Retrieve specific parameters
+const nameByParams = params.get('name'); // "John"
+
+userNameFind.innerHTML = nameByParams;
+// Step 5: Use the parameters as needed
